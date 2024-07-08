60f4acb9449c919fa956ab652b16146486efdebb
natista99 <natista99@gmail.com>
2024-07-08 11:13:50 +0900

    07_08

---
+++ javaproject/src/p2024_07_01/Test.java
@@ -0,0 +1,13 @@
+package p2024_07_01;
+
+public class Test {
+
+	// main() 메소드는 자바가상머신(JVM, java.exec)이 가장 먼저 호출하는 메소드이다
+	// 개발자가 직접 호출 불가
+	public static void main(String[] args) {
+		// 접근제한자 default private protected public
+		System.out.println("hello word");
+		
+	}
+
+}
